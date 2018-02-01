8823cb3ed0527a0093b41746f00e1d93a94cb87d
Lencho, Segni <segni.lencho@intelligrated.com>
2018-02-01 12:10:27 -0500

    User Class added

---
+++ src/main/java/com/sec/domain/User.java
@@ -0,0 +1,58 @@
+package com.sec.domain;
+
+import java.util.Objects;
+
+public class User {
+        private String firstName;
+    private String lastName;
+    private String sex;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", sex='" + sex + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(firstName, user.firstName) &&
+                Objects.equals(lastName, user.lastName) &&
+                Objects.equals(sex, user.sex);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(firstName, lastName, sex);
+    }
+}
